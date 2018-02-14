b72a0c7e10a02c3b7e153028a11aaf772d1d7fb4
Jessica Libman <jlibman@gmail.com>
2018-02-13 19:29:05 -0800

    firestoreeee

--- capstone/src/Login.js
+++ capstone/src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import firebase from 'firebase';
-import {auth, provider} from './client';
+import {auth, provider, db} from './FirestoreConfig';
 
 class Login extends React.Component {
   constructor(props){
@@ -13,6 +13,20 @@ class Login extends React.Component {
   async login() {
     const result = await auth().signInWithPopup(provider)
     this.setState({user: result.user});
+    // var users = db.CollectionReference('users');
+    
+    // users.doc('kUEypqhlHUFAn7lJcK6t').set({
+    //   FName: "sarah",
+    //   LName: "F",
+    //   DOB:'march 5 2020'
+    // })
+    // .then(function() {
+    //   console.log("Document successfully written!");
+    // })
+    // .catch(function(error) {
+    //     console.error("Error writing document: ", error);
+    // });
+
   }
 
   logout() {
@@ -20,6 +34,24 @@ class Login extends React.Component {
     this.setState({user: null});
   }
 
+
+
+
+  test() {
+    var users = db.collection("users");
+    
+    users.doc('new').set({
+      FName: "sarah",
+      LName: "F",
+      DOB:'march 5 2020'
+    })
+    .then(function() {
+      console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+  }
 
 render() {
   const {user} = this.state
@@ -29,6 +61,10 @@ render() {
             <p>{user ? `Hi, ${user.displayName}!` : 'Hi!'}</p>
         <button onClick={this.login.bind(this)}>
           Login with Facebook
+        </button>
+
+        <button onClick={this.test.bind(this)}>
+          TESTTESTTEST
         </button>
 
         <button onClick={this.logout.bind(this)}>
