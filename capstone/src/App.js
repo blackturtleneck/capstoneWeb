f5555cfec7b31abd35c194e456c15aff58bd953b
Jessica Libman <jlibman@gmail.com>
2018-02-23 15:44:48 -0800

    get user info from fb auth

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -21,10 +21,16 @@ class App extends React.Component {
     async login() {
       const result = await auth().signInWithPopup(provider)
       this.setState({ user: result.user });
-      console.log("results", result.user.email);
-      // Add a new document in collection "cities"
+      // Add a new document in collection "cities"n
       db.collection("users").doc(result.user.email).set({
         name: result.user.displayName,
+        fName: result.additionalUserInfo.profile.first_name,
+        lName: result.additionalUserInfo.profile.last_name,
+        gender: result.additionalUserInfo.profile.gender,
+        age: result.additionalUserInfo.profile.age_range.min,
+        linkFB: result.additionalUserInfo.profile.link,
+        timeZone: result.additionalUserInfo.profile.timezone,
+        photoURL: result.user.photoURL
       })
         .then(function () {
           console.log("Document successfully written!");
@@ -64,8 +70,7 @@ class App extends React.Component {
                     )
                 :
                 <div></div>
-                }
-                    
+                }        
                 
             </div>
         );
