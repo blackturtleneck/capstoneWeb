c9b7fc9894e9d38deb32c79412768a4ce437257a
sarahfeldmann <sarahf95@gmail.com>
2018-03-02 09:46:14 -0800

    make messaging work

--- capstone/src/Messenger.js
+++ capstone/src/Messenger.js
@@ -17,13 +17,18 @@ class Messenger extends React.Component {
       userEmail: this.props.userEmail,
       otherUser: this.props.otherUser
     }
-    db.collection("users").doc(this.state.user).collection("messages").doc(this.state.otherUser).set({0:""});
-    db.collection("users").doc(this.state.otherUser).collection("messages").doc(this.state.user).set({0:""});
+    // create a collection of message documents between this user and another user
+    db.collection("users").doc(this.props.user).collection("messages").doc(this.state.otherUser).collection("messages").doc("0").set({"from":null, id:0, text:""});
+    db.collection("users").doc(this.props.otherUser).collection("messages").doc(this.state.user).collection("messages").doc("0").set({"from":null, id:0, text:""});
+    ;
+  
 
+    this.updateMessage = this.updateMessage.bind(this)
+    this.submitMessage = this.submitMessage.bind(this)
   }
 
   componentDidMount() {
-    console.log("componentDidMout")
+    // console.log("componentDidMout")
     // firebase.database().ref('messages/').on('value', (snapshot) => {
     //   const currentMessages = snapshot.val()
 
@@ -35,19 +40,43 @@ class Messenger extends React.Component {
 
     // })
     let currentComponent = this;
-    let curUserList = [];
-    db.collection("users").get().then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
-        // doc.data() is never undefined for query doc snapshots
-        curUserList.push({"email":doc.id, "name":doc.get("name")});
-        console.log("doc.itd", doc.id + " "+ doc.get("name"))
-      });
-      console.log("curUserList", curUserList);
-      currentComponent.setState({userList:curUserList})
+    let curMessage = []
 
+    db.collection("users").doc(this.props.user).collection("messages").doc(this.state.otherUser).collection("messages")
+    .onSnapshot(function(querySnapshot) {
+      var curMessages = [];
+      querySnapshot.forEach(function(doc) {
+          curMessages.push(doc.data());
+      });
+    // .onSnapshot(function (doc) {
+    //   console.log("doc data", doc.data());
+    //   for (let i = 0; i < doc.data().length; i++) {
+    //     console.log("data", doc.data()[i])
+    //     curMessage.push(doc.data()[i])
+    //   }
+      currentComponent.setState({ messages: curMessages });
     });
-    console.log("this.state", this.state)
+    // querySnapshot.forEach(function(doc) {
+    //     // doc.data() is never undefined for query doc snapshots
+    //     console.log(doc.id, " => ", doc.data());
+    // });
+
   }
+
+
+  //   db.collection("users").doc(this.props.user).collection("messages").doc(this.state.otherUser).get().then(function (querySnapshot) {
+  //     querySnapshot.forEach(function (doc) {
+  //       console.log("this worked?!")
+  //       // doc.data() is never undefined for query doc snapshots
+  //       curUserList.push({ "email": doc.id, "name": doc.get("name") });
+  //       console.log("doc.itd", doc.id + " " + doc.get("name"))
+  //     });
+  //     console.log("curUserList", curUserList);
+  //     currentComponent.setState({ userList: curUserList })
+
+  //   });
+  //   console.log("this.state", this.state)
+  // }
 
   updateMessage(event) {
     console.log('updateMessage:' + event.target.value);
@@ -57,12 +86,18 @@ class Messenger extends React.Component {
   }
 
   submitMessage(event) {
+    const thisUser = db.collection("users").doc(this.props.user).collection("messages").doc(this.state.otherUser).collection("messages");
+    const otherUser = db.collection("users").doc(this.props.otherUser).collection("messages").doc(this.state.user).collection("messages");
+
     console.log('submitMessage: ' + this.state.message)
     const nextMessage = {
       id: this.state.messages.length,
-      text: this.state.message
+      text: this.state.message,
+      from: this.state.user
     }
-    firebase.database().ref('messages/' + nextMessage.id).set(nextMessage)
+    // firebase.database().ref('messages/' + nextMessage.id).set(nextMessage)
+    db.collection("users").doc(this.props.user).collection("messages").doc(this.state.otherUser).collection("messages").doc(this.state.messages.length + "").set(nextMessage);
+    db.collection("users").doc(this.props.otherUser).collection("messages").doc(this.state.user).collection("messages").doc(this.state.messages.length + "").set(nextMessage);
   }
 
   render() {
@@ -78,6 +113,7 @@ class Messenger extends React.Component {
           {currentMessage}
         </ol>
         <input onChange={this.updateMessage} type="text" placeholder="message" />
+        <button onClick={this.submitMessage}>Submit Message</button>
         <br />
       </div>
     );
