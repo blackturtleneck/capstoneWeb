c6306a034db34c31a8108c922afc26491f7cce2c
sarahfeldmann <sarahf95@gmail.com>
2018-04-28 22:57:13 -0700

    add second page but can't get values

--- capstone/src/SignUp/SignUp2.js
+++ capstone/src/SignUp/SignUp2.js
@@ -35,7 +35,7 @@ class SignUp2 extends React.Component {
         <Tooltip
           prefixCls="rc-slider-tooltip"
           overlay={value}
-          visible={dragging}
+          visible={true}
           placement="top"
           key={index}
         >
@@ -56,39 +56,75 @@ class SignUp2 extends React.Component {
           <div className="next-step next">
             <input type="image" className="next next-2" src={next} />
           </div>
-          <label className="signup-label" for="match-gender">
+          <label className="signup-label" for="matchGender">
             I'M LOOKING FOR...
           </label>
           <select
-            className="custom-select"
-            name="match-gender"
+            required
+            className="custom-select margin"
+            name="matchGender"
             value={this.props.fieldValues.matchGender}
           >
             <option selected />
             <option value="male">MALE</option>
-            <option value="female">FEAMLE</option>
+            <option value="female">FEMALE</option>
           </select>
 
           <label className="signup-label" for="match-age">
             AGE
           </label>
-          <Range
-            min={18}
-            max={55}
-            defaultValue={[25, 30]}
-            handle={handle}
-            trackStyle={[
-              { backgroundColor: "#828282" },
-              { backgroundColor: "#828282" }
-            ]}
-            handleStyle={[
-              { backgroundColor: "#9BA2FF", borderColor: "#9BA2FF" },
-              { backgroundColor: "#9BA2FF", borderColor: "#9BA2FF" }
-            ]}
-          />
+          <div id="ageRange">
+            <Range
+              min={18}
+              max={55}
+              defaultValue={[25, 30]}
+              handle={handle}
+              trackStyle={[
+                { backgroundColor: "#828282" },
+                { backgroundColor: "#828282" }
+              ]}
+              handleStyle={[
+                { backgroundColor: "#9BA2FF", borderColor: "#9BA2FF" },
+                { backgroundColor: "#9BA2FF", borderColor: "#9BA2FF" }
+              ]}
+              onChange={v => this.handleChange("name", v)}
+              //     console.log(this.state.maxValue);
+              //   }
+            />
+          </div>
+          <label className="signup-label distance-wrapper" for="distance">
+            DISTANCE
+          </label>
+          <div id="distance">
+            <Slider
+              min={0}
+              max={50}
+              defaultValue={10}
+              handle={handle}
+              trackStyle={[{ backgroundColor: "#828282" }]}
+              handleStyle={[
+                { backgroundColor: "#9BA2FF", borderColor: "#9BA2FF" }
+              ]}
+              onChange={v => this.handleChange("name", v)}
+            />
+          </div>
         </form>
       </div>
     );
+  }
+
+  nextStep(e) {
+    let currentComponent = this;
+    e.preventDefault();
+    console.log("document.getelemnt", document.getElementsByTagName("Range"));
+    var data = {
+      matchGender: e.target.matchGender.value,
+      ageRange: e.target.ageRange.value,
+      distance: e.target.distance.value
+    };
+    console.log("data", data);
+    currentComponent.props.saveValues(data);
+    currentComponent.props.nextStep();
   }
 }
 
