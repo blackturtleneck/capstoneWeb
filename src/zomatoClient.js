41ccb51a4552be92fded4324f13f281d5f8a28bc
sarahfeldmann <sarahf95@gmail.com>
2018-04-30 00:19:00 -0700

    lint til i die

--- src/zomatoClient.js
+++ src/zomatoClient.js
@@ -1,23 +1,26 @@
+/*global client:true*/
+/*eslint no-undef: "error"*/
 
 // Initialize zomato
-    var zomato = require('zomato');
+var zomato = require('zomato');
 
 const config = {
-    userKey: '1b8bf5702241de94be0cba6b8772d29e',  
-
+    userKey: '1b8bf5702241de94be0cba6b8772d29e'
 };
 
-
-      client.getCities({
-        q:"New Delhi", //query by city name
-        lat:"28.613939", //latitude
-        lon:"77.209021", //longitude
-        city_ids:"1,2,3", //comma separated city_ids value
-        count:"2" // number of maximum result to display
-        }, function(err, result){
-            if(!err){
-              console.log(result);
-            }else {
-              console.log(err);
-            }
-        });
+client.getCities(
+    {
+        q: 'New Delhi', //query by city name
+        lat: '28.613939', //latitude
+        lon: '77.209021', //longitude
+        city_ids: '1,2,3', //comma separated city_ids value
+        count: '2' // number of maximum result to display
+    },
+    function(err, result) {
+        if (!err) {
+            console.log(result);
+        } else {
+            console.log(err);
+        }
+    }
+);
