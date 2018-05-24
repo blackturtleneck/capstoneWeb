ec5ac56fa7de3bf60be6db95d448c6ba24628375
Jessica Libman <jlibman@gmail.com>
2018-05-24 14:08:45 -0700

    displays other users pictures

--- src/Profile/sideProf.js
+++ src/Profile/sideProf.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { db } from '../FirestoreConfig';
 import './Profile.css';
 import placeholder from './placeholder.svg'
+import './sideProf.css'
 
 class SideProf extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             userDoc: '',
-            newProps: ''
+            newProps: '',
+            images: ''
         }
     }
 
@@ -29,13 +31,10 @@ class SideProf extends React.Component {
                     }
                 });
         }
-        console.log(this.state.userDoc.photos.data)
-        let images;
+        let images = [];
         {
-            this.state.userDoc.photos.data.map((photo, i) => {
-                images.push(
-                    <p key={i}>{photo.id}</p>)
-
+            this.state.userDoc.photos.data.forEach(function (photo) {
+                images.push(photo);
             })
         }
         this.setState({ images: images });
@@ -55,19 +54,34 @@ class SideProf extends React.Component {
                     console.log('No such document!');
                 }
             });
+        if (this.state.userDoc !== '') {
+            let images = [];
+            {
+                this.state.userDoc.photos.data.forEach(function (photo) {
+                    images.push(photo);
+                })
+            }
+            this.setState({ images: images });
+        }
     }
 
     render() {
-        console.log(this.state.images)
         return (
-            <div>
-
+            <div className="otherProfile">
                 <p>{this.state.userDoc.name}</p>
                 <p> {this.state.userDoc.age}</p>
                 <p> {this.state.userDoc.gender}</p>
                 <p> {this.state.userDoc.photoURL}</p>
                 <img src={placeholder} alt="pic placeholder" />
-                {this.state.images}
+                {this.state.images !== '' ? (
+                    this.state.images.map((photo, i) => {
+                        return (
+                            // <p key={i}>{photo.id}</p>
+                            <div><img src={placeholder} alt="pic placeholder" /></div>);
+                    })
+                ) : (
+                        <p>no images</p>
+                    )}
 
 
             </div>
