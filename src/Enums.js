de59b470d64aa9998a8046bf727d4b002e5a88b9
Jessica Libman <jlibman@gmail.com>
2018-05-30 01:45:28 -0700

    added icon classnames

--- src/Enums.js
+++ src/Enums.js
@@ -13,6 +13,33 @@ exports.PageContent = {
 exports.Routes = {
     MESSENGER: 'messenger',
     PROFILE: 'profile',
-    DATE_SELECTION: 'date-selection', 
+    DATE_SELECTION: 'date-selection',
     SIGN_UP: 'signup'
 };
+
+exports.Interests = {
+    ANIMALS: "fas fa-paw",
+    TRAVEL: "fas fa-plane",
+    FOOD: "fas fa-utensils",
+    MUSIC: "fas fa-music",
+    SPORTS: "fas fa-futbol",
+    MOVIES: "fas fa-film",
+    TECH: "fas fa-mobile",
+    GAMING: "fas fa-gamepad",
+    NATURE: "fas fa-tree",
+    COFFEE: "fas fa-coffee",
+    DRINKS: "fas fa-glass-martini",
+    DINNER: "fas fa-utensils",
+    MUSEUM: "fas fa-institution",
+    SHOW: "fas fa-ticket",
+    PARK: "fas fa-tree"
+};
+
+exports.FirstDates = {
+    coffee: "fas fa-coffee",
+    drinks: "fas fa-glass-martini",
+    dinner: "fas fa-utensils",
+    museum: "fas fa-university",
+    show: "fas fa-ticket-alt",
+    park: "fas fa-tree"
+}
