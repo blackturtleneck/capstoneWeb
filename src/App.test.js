f25a79199ba4650a35ba92552548a599efd7747b
Joycie Yu <joycie@uw.edu>
2018-05-29 19:20:01 -0700

    maybe it fixed

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("renders without crashing", () => {
+    // eslint-disable-next-line no-use-before-define
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
